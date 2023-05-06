71572f9b1e5a02d45a8faa677708f1fffff8ad97
osiacteodora <113546509+osiacteodora@users.noreply.github.com>
2023-05-06 16:42:50 +0300

    UserType

--- DataAccess/DTOs/SessionDataDto.cs
+++ DataAccess/DTOs/SessionDataDto.cs
@@ -4,6 +4,7 @@
     {
         public string SessionId { get; set; }
         public string UserId { get; set; }
+        public string UserType { get; set; }
         public string BearerToken { get; set; }
     }
 }
--- RestServices/Services/SessionService.cs
+++ RestServices/Services/SessionService.cs
@@ -1,22 +1,25 @@
 ﻿using RestaurantAppBE.DataAccess.Constants;
 using RestaurantAppBE.DataAccess.DTOs;
+using RestaurantAppBE.DataAccess.Enums;
 
 namespace RestaurantAppBE.RestServices.Services
 {
     public class SessionService: ISessionService
     {
-        public SessionDataDto GetSessionInfo(HttpContext httpContext, int? userId = default, string? sessionBearer = default)
+        public SessionDataDto GetSessionInfo(HttpContext httpContext, int? userId = default, UserType userType=default, string? sessionBearer = default)
         {
             var sessionInfo = new SessionDataDto { };
             if (string.IsNullOrEmpty(httpContext.Session.GetString(SessionVariables.SessionKeyId)) || sessionBearer != null)
             {
                 httpContext.Session.SetString(SessionVariables.SessionKeyId, Guid.NewGuid().ToString());
                 httpContext.Session.SetString(SessionVariables.SessionUserId, userId.ToString());
+                httpContext.Session.SetString(SessionVariables.SessionUserType, userType.ToString());
                 httpContext.Session.SetString(SessionVariables.SessionBearerToken, sessionBearer);
             }
 
             sessionInfo.SessionId = httpContext.Session.GetString(SessionVariables.SessionKeyId);
             sessionInfo.UserId = httpContext.Session.GetString(SessionVariables.SessionUserId);
+            sessionInfo.UserType = httpContext.Session.GetString(SessionVariables.SessionUserType);
             sessionInfo.BearerToken = httpContext.Session.GetString(SessionVariables.SessionBearerToken);
 
             return sessionInfo;
